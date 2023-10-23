63cd38ea221b902642db84e00029510d275a7f3f
magnetar-star <matthew.lvk@gmail.com>
2023-10-23 20:11:04 +0200

    update

---
+++ mc_examples/mc_examples/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mc_examples
+//
+//  Created by Matthew liew on 23.10.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
