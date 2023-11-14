a5be7e66a914685a6f3e0393dc35b147169d20f6
magnetar-star <matthew.lvk@gmail.com>
2023-11-14 15:27:46 +0100

    update

---
+++ random_generator/brownian_motion_generator.cpp
@@ -0,0 +1,16 @@
+//
+//  brownian_motion_generator.cpp
+//  mc_examples
+//
+//  Created by Matthew liew on 30.10.23.
+//
+
+#include "brownian_motion_generator.hpp"
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+
+std::vector<double> brownian_motion_generator(std::vector<int> &time) {
+    
+}
---
+++ random_generator/brownian_motion_generator.hpp
@@ -0,0 +1,13 @@
+//
+//  brownian_motion_generator.hpp
+//  mc_examples
+//
+//  Created by Matthew liew on 30.10.23.
+//
+
+#ifndef brownian_motion_generator_hpp
+#define brownian_motion_generator_hpp
+
+std::vector<double> brownian_motion_generator(std::vector<int> &time);
+
+#endif /* brownian_motion_generator_hpp */
